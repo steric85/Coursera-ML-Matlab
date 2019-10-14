954dba8ac8ed62db255daa22e87f60e7393180ab
Steric <steric85@users.noreply.github.com>
2019-10-15 00:10:15 +0530

    Add solutions for ex3

---
+++ ex3/predict.m
@@ -0,0 +1,37 @@
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+%
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
+%
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];     %m x (n+1)
+
+%Layer 2
+A_2 = [ones(m, 1) sigmoid(X * Theta1')]; % m x 26
+
+%Layer 3
+A_3 = sigmoid(A_2 * Theta2'); % m x num_labels
+
+[maxVal, p] = max(A_3, [], 2);
+% =========================================================================
+
+
+end
